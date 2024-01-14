80973495c82562313bbe4b60cf239af7aaf0dc6d
Iryna Akulova <Iryna_Akulova@epam.com>
2024-01-14 22:11:49 +0200

    Task 2

---
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -0,0 +1,49 @@
+package com.task02;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.syndicate.deployment.annotations.LambdaUrlConfig;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@LambdaHandler(lambdaName = "hello_world",
+	roleName = "hello_world-role",
+	isPublishVersion = true
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+	private static final int SC_OK = 200;
+	private static final int SC_BAD_REQUEST = 400;
+	private final Gson gson = new Gson();
+
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent, Context context) {
+		context.getLogger().log(apiGatewayProxyRequestEvent.toString());
+		try {
+			Map<String, Object> responseBody = new LinkedHashMap<>();
+			responseBody.put("message", "Hello from Lambda");
+
+			return new APIGatewayProxyResponseEvent()
+					.withStatusCode(SC_OK)
+					.withBody(gson.toJson(responseBody));
+		} catch (IllegalArgumentException exception) {
+			return new APIGatewayProxyResponseEvent()
+					.withStatusCode(SC_BAD_REQUEST)
+					.withBody(exception.toString());
+		}
+	}
+}
