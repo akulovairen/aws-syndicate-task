dbe7139f137f1614ed31f987f88e6b57db867711
Iryna Akulova <Iryna_Akulova@epam.com>
2024-02-01 16:12:46 +0200

    Task 10

--- task10/jsrc/main/java/com/task10/model/Reservations.java
+++ task10/jsrc/main/java/com/task10/model/Reservations.java
@@ -2,13 +2,13 @@ package com.task10.model;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBIndexHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
 @DynamoDBTable(tableName = "cmtr-804a9f76-Reservations-test")
 public class Reservations {
-	@DynamoDBHashKey(attributeName = "id")
-	private String id;
+//	@DynamoDBHashKey(attributeName = "id")
+//	private String id;
+	@DynamoDBHashKey(attributeName = "tableNumber")
 	private int tableNumber;
 	private String clientName;
 	private String phoneNumber;
@@ -16,16 +16,16 @@ public class Reservations {
 	private String slotTimeStart;
 	private String slotTimeEnd;
 
-	@DynamoDBHashKey(attributeName = "id")
-	public String getId() {
-		return id;
-	}
+//	@DynamoDBHashKey(attributeName = "id")
+//	public String getId() {
+//		return id;
+//	}
+//
+//	public void setId(String id) {
+//		this.id = id;
+//	}
 
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	@DynamoDBAttribute(attributeName = "tableNumber")
+	@DynamoDBHashKey(attributeName = "tableNumber")
 	public int getTableNumber() {
 		return tableNumber;
 	}
@@ -82,8 +82,7 @@ public class Reservations {
 	@Override
 	public String toString() {
 		return "Reservations{" +
-				"id='" + id + '\'' +
-				", tableNumber=" + tableNumber +
+				" tableNumber=" + tableNumber +
 				", clientName='" + clientName + '\'' +
 				", phoneNumber='" + phoneNumber + '\'' +
 				", date='" + date + '\'' +
